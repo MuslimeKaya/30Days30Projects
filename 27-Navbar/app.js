a51ec9cab37812a4623094567503651e287ce4fd
Müslime Kayar <muslimekayar2121@gmail.com>
2025-04-25 14:36:19 +0300

    Add files via upload

---
+++ 27-Navbar/app.js
@@ -0,0 +1,14 @@
+
+const nav = document.querySelector('.nav')
+
+const add = 150
+
+window.addEventListener('scroll', () => {
+
+
+    if (window.scrollY > nav.offsetHeight + add) {
+        nav.classList.add('move')
+    } else {
+        nav.classList.remove('move')
+    }
+})
